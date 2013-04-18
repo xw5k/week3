d9c73ae6faa0d70f1129e675e091926fb32a93e6
Jeff Cohen <jeff@starterleague.com>
2013-04-18 10:13:29 -0500

    Start freecyle app

---
+++ lecture_code/freecycle/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freecycle::Application.config.secret_token = 'c81125056a26c1cf71748abc5db16e0758c801e886f9dc7c9497248bb4bc5a3732bf2763a4e5ca5d159b79f93ce2defd602e275a35c11dc2b2d45b9a83df66b9'
