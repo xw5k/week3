5bdd428bc3db843af0181c627f21faed7cc21b55
Jeff Cohen <jeff@starterleague.com>
2013-04-18 14:40:29 -0500

    Improve the names of things

--- lecture_code/classes.rb
+++ lecture_code/classes.rb
@@ -1,38 +1,47 @@
-# jeff = { "name" => "Jeff Cohen", "id" => "1001", "expires" => "12/31/13"}
-# bob = { "name" => "Bob Smith", "id" => "4253", "expires" => "12/31/14"}
+# jeff = { :name => "Jeff Cohen", :id => "1001", :expires => "12/31/13"}
+# bob = { :name => "Bob Smith", :id => "4253", :expires => "12/31/14"}
 
 # def display_id_card(card_type, person_info)
-#   puts "Card Type: #{2 + 5}"
+#   puts "Card Type: #{card_type}"
 #   puts "Name: #{person_info[:name]}"
 #   puts "ID#: #{person_info[:id]}"
 #   puts "Expires: #{person_info[:expires]}"
+#   return nil
 # end
 
-# # display_id_card "staff", jeff
+# display_id_card "staff", bob
 
-class Person
+class IdentificationCard
   attr_accessor :id
-  attr_accessor :name
+  attr_accessor :first_name
+  attr_accessor :last_name
   attr_accessor :expires
   attr_accessor :role
+
+  def full_name
+    return "#{last_name}, #{first_name}"
+  end
+
 end
 
-def display_id_card(person_info)
-  puts "| Card Type: #{person_info.role}"
-  puts "| Name: #{person_info.name}"
-  puts "| ID#: #{person_info.id}"
-  puts "| Expires: #{person_info.expires}"
+def display_id_card(id_card)
+  puts "| Card Type: #{id_card.role}"
+  puts "| Name: #{id_card.full_name}"
+  puts "| ID#: #{id_card.id}"
+  puts "| Expires: #{id_card.expires}"
   puts "-" * 30
 end
 
-person1 = Person.new
-person1.name = "Jeff Cohen"
+person1 = IdentificationCard.new
+person1.first_name = "Jeff"
+person1.last_name = "Cohen"
 person1.id = "1001"
 person1.expires = "12/31/2013"
 person1.role = "Alumni"
 
-person2 = Person.new
-person2.name = "Bob Smith"
+person2 = IdentificationCard.new
+person2.first_name = "Bob"
+person2.last_name = "Smith"
 person2.id = "4253"
 person2.expires = "12/31/2014"
 person2.role = "Student"
